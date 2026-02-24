078d1f4e95de4e9fe7cc9f1df48feac8c83531b8
yuke <yusukesaikyou931@gmail.com>
2026-02-24 17:07:27 +0900

    test: add get home, help, about page

--- ch_3/spec/requests/static_pages_spec.rb
+++ ch_3/spec/requests/static_pages_spec.rb
@@ -1,18 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe "StaticPages", type: :request do
-  describe "GET /home" do
-    it "returns http success" do
-      get "/static_pages/home"
-      expect(response).to have_http_status(:success)
+RSpec.describe 'StaticPages', type: :request do
+  let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
+
+  describe 'GET /home' do
+    before { get root_path }
+
+    it 'HTTPステータス200を返すこと' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it '適切なタイトルが表示されること' do
+      expect(response.body).to include("<title>#{base_title}</title>")
     end
   end
 
-  describe "GET /help" do
-    it "returns http success" do
-      get "/static_pages/help"
-      expect(response).to have_http_status(:success)
+  describe 'GET /help' do
+    before { get help_path }
+
+    it 'HTTPステータス200を返すこと' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it '適切なタイトルが表示されること' do
+      expect(response.body).to include("<title>Help | #{base_title}</title>")
     end
   end
 
+  describe 'GET /about' do
+    before { get about_path }
+
+    it 'HTTPステータス200を返すこと' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it '適切なタイトルが表示されること' do
+      expect(response.body).to include("<title>About | #{base_title}</title>")
+    end
+  end
 end
